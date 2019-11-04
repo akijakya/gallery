13fcec4a211a67a52007a5ef35982345334e319e
András Jáky <jakyandras@gmail.com>
2019-11-04 12:20:52 +0100

    refactored the code to be more logical and structured

--- gallery.js
+++ gallery.js
@@ -13,97 +13,184 @@ let images = [
     {src: "static\\img\\IMG_20191017_112516.jpg", alt: "Sirok", description: "The view on the village of Sirok from the castle"}
 ]
 
-let currentPicture = document.getElementsByClassName("picture-box")[0].getElementsByTagName("img")[0];
-let currentIndex = 0;
-
-function showPicture (x) {
-    currentIndex = x;
-
-    let pictureBox = document.getElementsByClassName("picture-box")[0];
-
-    if (currentPicture !== undefined) {
-        pictureBox.removeChild(pictureBox.getElementsByTagName("img")[0]);
-        pictureBox.removeChild(pictureBox.getElementsByClassName("text-box")[0]);
-    }
-    
+function addNewPicture (index, classToSet) {
     let newPicture = document.createElement("img");
-    newPicture.setAttribute("src", images[x].src);
-    newPicture.setAttribute("alt", images[x].alt);
-    newPicture.setAttribute("data-desc", images[x].description);
-    newPicture.setAttribute("class", "picture");
-    newPicture.setAttribute("data-index", x);
 
-    if (images[x].needRotation === true) {
+    newPicture.setAttribute("src", images[index].src);
+    newPicture.setAttribute("class", classToSet);
+    newPicture.setAttribute("id", index);
+
+    if (images[index].needRotation === true) {
         newPicture.setAttribute("style", "transform: rotate(90deg)");
     }
 
-    pictureBox.appendChild(newPicture);
-    currentPicture = newPicture;
+    return newPicture;
+}
+
+function showBigPicture (index) {
+    let currentPicture = document.getElementsByClassName("clicked")[0];
+    currentPicture.setAttribute("class", "notclicked");
+    let clickedPicture = document.getElementById(index);
+    clickedPicture.setAttribute("class", "clicked");
+
+    let pictureBox = document.getElementsByClassName("picture-box")[0];
+
+    if (pictureBox.getElementsByTagName("img")[0] !== undefined) {
+        pictureBox.removeChild(pictureBox.getElementsByTagName("img")[0]);
+        pictureBox.removeChild(pictureBox.getElementsByClassName("text-box")[0]);
+    }
+
+    pictureBox.appendChild(addNewPicture(index, "picture"));
 
     let textBox = document.createElement("div");
     textBox.setAttribute("class", "text-box");
     
     let newHeader = document.createElement("h1");
-    newHeader.textContent = images[x].alt;
+    newHeader.textContent = images[index].alt;
     let newParagraph = document.createElement("p");
-    newParagraph.textContent = images[x].description;
+    newParagraph.textContent = images[index].description;
 
     textBox.appendChild(newHeader);
     textBox.appendChild(newParagraph);
     pictureBox.appendChild(textBox);
 }
 
-showPicture(currentIndex);
+function pageLoad () {
+    // adding thumbnails with onlcick event
+    let thumbnailList = document.getElementById("picture-list").getElementsByTagName("ul")[0];
 
-let thumbnailList = document.getElementById("picture-list").getElementsByTagName("ul")[0];
-let clickedThumbnail;
-
-for (let i = 0; i < images.length; i++) {
-    let thumbnail = document.createElement("img");
-    thumbnail.setAttribute("src", images[i].src);
-    thumbnail.setAttribute ("class", "notclicked");
-    thumbnail.setAttribute ("id", i);
-
-    if (images[i].needRotation === true) {
-        thumbnail.setAttribute("style", "transform: rotate(90deg)");
-    }
-
-    if (i === 0) {
-        clickedThumbnail = thumbnail;
-        clickedThumbnail.setAttribute ("class", "clicked");
-    }
-
-    let newLi = document.createElement("li");
-    newLi.appendChild(thumbnail);
+    for (let i = 0; i < images.length; i++) {
+        let thumbnail;
+        if (i === 0) {
+            thumbnail = addNewPicture(i, "clicked");
+        } else {
+            thumbnail = addNewPicture(i, "notclicked");
+        }
     
-    thumbnailList.appendChild(newLi);
-
-    thumbnail.onclick = function() {
-        clickedThumbnail.setAttribute ("class", "notclicked");
-        clickedThumbnail = thumbnail;
-        clickedThumbnail.setAttribute ("class", "clicked");
-        showPicture(i);
+        let newLi = document.createElement("li");
+        newLi.appendChild(thumbnail);
+        thumbnailList.appendChild(newLi);
+    
+        thumbnail.onclick = function () {
+            showBigPicture(i);
+        }
     }
+
+    //adding the big picture
+    showBigPicture(0);
 }
 
 let leftArrow = document.getElementById("arrow-left");
 leftArrow.onclick = function() {
-	if (currentPicture.dataset.index > 0) {
-        let thumbnailIndex = parseInt(clickedThumbnail.id);
-        clickedThumbnail.setAttribute ("class", "notclicked");
-        clickedThumbnail = thumbnailList.getElementsByTagName("li")[thumbnailIndex - 1].getElementsByTagName("img")[0];
-        clickedThumbnail.setAttribute ("class", "clicked");
-		showPicture(currentIndex - 1);
+    let currentPicture = document.getElementsByClassName("clicked")[0];
+	if (parseInt(currentPicture.id) > 0) {
+        showBigPicture(parseInt(currentPicture.id) - 1);
 	} 
 }
 
 let rightArrow = document.getElementById("arrow-right");
 rightArrow.onclick = function() {
-	if (currentPicture.dataset.index < images.length - 1) { 
-        let thumbnailIndex = parseInt(clickedThumbnail.id);
-        clickedThumbnail.setAttribute ("class", "notclicked");
-        clickedThumbnail = thumbnailList.getElementsByTagName("li")[thumbnailIndex + 1].getElementsByTagName("img")[0];
-        clickedThumbnail.setAttribute ("class", "clicked");
-        showPicture(currentIndex + 1);
+    let currentPicture = document.getElementsByClassName("clicked")[0];
+	if (parseInt(currentPicture.id) < images.length - 1) { 
+        showBigPicture(parseInt(currentPicture.id) + 1);
 	} 
 }
+
+window.onload = function () {
+    pageLoad();
+}
+
+// let currentPicture = document.getElementsByClassName("picture-box")[0].getElementsByTagName("img")[0];
+// let currentIndex = 0;
+
+// function showPicture (x) {
+//     currentIndex = x;
+
+//     let pictureBox = document.getElementsByClassName("picture-box")[0];
+
+//     if (currentPicture !== undefined) {
+//         pictureBox.removeChild(pictureBox.getElementsByTagName("img")[0]);
+//         pictureBox.removeChild(pictureBox.getElementsByClassName("text-box")[0]);
+//     }
+    
+//     let newPicture = document.createElement("img");
+//     newPicture.setAttribute("src", images[x].src);
+//     newPicture.setAttribute("alt", images[x].alt);
+//     newPicture.setAttribute("data-desc", images[x].description);
+//     newPicture.setAttribute("class", "picture");
+//     newPicture.setAttribute("data-index", x);
+
+//     if (images[x].needRotation === true) {
+//         newPicture.setAttribute("style", "transform: rotate(90deg)");
+//     }
+
+//     pictureBox.appendChild(newPicture);
+//     currentPicture = newPicture;
+
+//     let textBox = document.createElement("div");
+//     textBox.setAttribute("class", "text-box");
+    
+//     let newHeader = document.createElement("h1");
+//     newHeader.textContent = images[x].alt;
+//     let newParagraph = document.createElement("p");
+//     newParagraph.textContent = images[x].description;
+
+//     textBox.appendChild(newHeader);
+//     textBox.appendChild(newParagraph);
+//     pictureBox.appendChild(textBox);
+// }
+
+// showPicture(currentIndex);
+
+// let thumbnailList = document.getElementById("picture-list").getElementsByTagName("ul")[0];
+// let clickedThumbnail;
+
+// for (let i = 0; i < images.length; i++) {
+//     let thumbnail = document.createElement("img");
+//     thumbnail.setAttribute("src", images[i].src);
+//     thumbnail.setAttribute ("class", "notclicked");
+//     thumbnail.setAttribute ("id", i);
+
+//     if (images[i].needRotation === true) {
+//         thumbnail.setAttribute("style", "transform: rotate(90deg)");
+//     }
+
+//     if (i === 0) {
+//         clickedThumbnail = thumbnail;
+//         clickedThumbnail.setAttribute ("class", "clicked");
+//     }
+
+//     let newLi = document.createElement("li");
+//     newLi.appendChild(thumbnail);
+    
+//     thumbnailList.appendChild(newLi);
+
+//     thumbnail.onclick = function() {
+//         clickedThumbnail.setAttribute ("class", "notclicked");
+//         clickedThumbnail = thumbnail;
+//         clickedThumbnail.setAttribute ("class", "clicked");
+//         showPicture(i);
+//     }
+// }
+
+// let leftArrow = document.getElementById("arrow-left");
+// leftArrow.onclick = function() {
+// 	if (currentPicture.dataset.index > 0) {
+//         let thumbnailIndex = parseInt(clickedThumbnail.id);
+//         clickedThumbnail.setAttribute ("class", "notclicked");
+//         clickedThumbnail = thumbnailList.getElementsByTagName("li")[thumbnailIndex - 1].getElementsByTagName("img")[0];
+//         clickedThumbnail.setAttribute ("class", "clicked");
+// 		showPicture(currentIndex - 1);
+// 	} 
+// }
+
+// let rightArrow = document.getElementById("arrow-right");
+// rightArrow.onclick = function() {
+// 	if (currentPicture.dataset.index < images.length - 1) { 
+//         let thumbnailIndex = parseInt(clickedThumbnail.id);
+//         clickedThumbnail.setAttribute ("class", "notclicked");
+//         clickedThumbnail = thumbnailList.getElementsByTagName("li")[thumbnailIndex + 1].getElementsByTagName("img")[0];
+//         clickedThumbnail.setAttribute ("class", "clicked");
+//         showPicture(currentIndex + 1);
+// 	} 
+// }
